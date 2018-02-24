3080a4d117e04bbfb2b6199036dadad6064c2352
Briand Sanderson <ab@chezbriand.com>
2018-02-23 23:20:24 -0800

    RPI Motion Sensor Device

---
+++ devicetypes/abchez/rpi-motion-sensor-device.src/rpi-motion-sensor-device.groovy
@@ -0,0 +1,48 @@
+/**
+ *  RPI Motion Sensor Device
+ *
+ *  Copyright 2018 Alvaro Miranda 
+ *
+ */
+metadata {
+	definition (name: "RPI Motion Sensor Device", namespace: "abchez", author: "Alvaro Miranda") {
+		capability "Motion Sensor"
+	}
+
+
+	simulator {
+		status "inactive": "motion:inactive"
+		status "active": "motion:active"
+	}
+
+	tiles(scale: 2) {
+    	
+		multiAttributeTile(name:"motion", type: "generic", width: 6, height: 4){
+			tileAttribute ("device.motion", key: "PRIMARY_CONTROL") {
+				attributeState "inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#ffffff"
+				attributeState "active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#00a0dc"
+			}
+		}
+		main "motion"
+		details "motion"
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	Log("parse ${description}")
+}
+
+def setActive(data) {
+	Log("setActive ${data}")
+	return sendEvent(name: "motion", value: "active", data : data)
+}
+
+def setInactive(data) {
+	Log("setInactive ${data}")
+    return sendEvent(name: "motion", value: "inactive", data : data)
+}
+
+def Log(String text) {
+    parent.Log(text)
+}
